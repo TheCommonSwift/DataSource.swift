bc605bcc24aebd2eed4b18208f1ed46e3b0d4842
Quico Moya <me@qmoya.com>
2015-09-29 13:11:10 +0200

    Initial Commit

---
+++ DataSource/DataSource.h
@@ -0,0 +1,19 @@
+//
+//  DataSource.h
+//  DataSource
+//
+//  Created by Quico Moya on 29/09/15.
+//  Copyright © 2015 Quico Moya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DataSource.
+FOUNDATION_EXPORT double DataSourceVersionNumber;
+
+//! Project version string for DataSource.
+FOUNDATION_EXPORT const unsigned char DataSourceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DataSource/PublicHeader.h>
+
+
