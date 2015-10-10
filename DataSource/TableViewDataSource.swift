6121e0c1ea5cc6fec77ef2deb56174dc538177a4
Björn Roßborsky-Stoß <bjoern.rossborsky@web.de>
2015-10-10 12:09:07 +0200

    add swiftLint style convention tool, and style code

--- DataSource/TableViewDataSource.swift
+++ DataSource/TableViewDataSource.swift
@@ -10,41 +10,41 @@ class TableViewDataSource<CellType: UITableViewCell>: NSObject, UITableViewDataS
 	private let delegate: TableViewDataSourceDelegate?
 	private let cellConfigurationHandler: (CellType, AnyObject) -> ()
     private let cellReuseIdentifier: String?
-    
-	init(tableView tv: UITableView, dataProvider dp: DataProvider, delegate d: TableViewDataSourceDelegate, cellConfigurationHandler h: (CellType, AnyObject) -> ()) {
+	init(tableView tv: UITableView, dataProvider dp: DataProvider,
+        delegate d: TableViewDataSourceDelegate,
+        cellConfigurationHandler h: (CellType, AnyObject) -> ()) {
 		tableView = tv
 		dataProvider = dp
 		delegate = d
 		cellConfigurationHandler = h
         cellReuseIdentifier = nil
 	}
-    
-    init(tableView tv: UITableView, dataProvider dp: DataProvider, cellReuseIdentifier id: String, cellConfigurationHandler h: (CellType, AnyObject) -> ()) {
+    init(tableView tv: UITableView, dataProvider dp: DataProvider, cellReuseIdentifier id: String,
+        cellConfigurationHandler h: (CellType, AnyObject) -> ()) {
         tableView = tv
         dataProvider = dp
         cellReuseIdentifier = id
         cellConfigurationHandler = h
         delegate = nil
     }
-	
     private func reuseIdentifierForObject(object: AnyObject) -> String {
         if let id = cellReuseIdentifier { return id }
         if let d = delegate, let id = d.cellIdentifierForObject(object) { return id }
-        fatalError("Either cellReuseIdentifier must be set or cellIdentifierForObject must return an identifier")
+        fatalError("Either cellReuseIdentifier must be set or cellIdentifierForObject must " +
+            "return an identifier")
     }
-    
 	func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
 		return dataProvider.numberOfRowsInSection(section)
 	}
-	
 	func numberOfSectionsInTableView(tableView: UITableView) -> Int {
 		return dataProvider.numberOfSections()
 	}
-	
-	func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+	func tableView(tableView: UITableView,
+        cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
 		let obj = dataProvider.objectAtIndexPath(indexPath)
 		let id = reuseIdentifierForObject(obj)
-		guard let cell = tableView.dequeueReusableCellWithIdentifier(id, forIndexPath: indexPath) as? CellType else { fatalError("The dequeued cell has the wrong type") }
+		guard let cell = tableView.dequeueReusableCellWithIdentifier(id, forIndexPath: indexPath)
+            as? CellType else { fatalError("The dequeued cell has the wrong type") }
         cellConfigurationHandler(cell, obj)
 		return cell
 	}
